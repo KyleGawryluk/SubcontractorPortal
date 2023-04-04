c663779fa6169ee5b9dd1f96902100b4034ad186
Kyle Gawryluk <kgawryluk@redglaze.com>
2023-04-04 10:30:29 -0500

    Update

--- config/api.php
+++ config/api.php
@@ -1,10 +1,20 @@
 <?php
 
+//localhost
+// return [
+//     'URL' => '192.168.101.137/2022R1/entity/',
+//     'INSTANCE' => '192.168.101.137/2022R1/',
+//     'TENANT'  =>'Company',
+//     'BRANCH'  =>'HEADOFFICE',
+//     'SECRET'=>'ElsbHv7K4ajhAngFL1L9Ow',
+//     'CLIENTID'=>'904E1E66-B558-6916-1210-B0921C85B288@Company',
+// ];
+
+
+//UAT
 return [
     'URL' => 'https://sghredglaze.acumatica.com/entity/',
     'INSTANCE' => 'https://sghredglaze.acumatica.com/',
-    'TENANT'  =>'Company',
-    'BRANCH'  =>'HEADOFFICE',
 ];
 
 
